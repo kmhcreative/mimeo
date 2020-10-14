a60a5a9ee9ea38a17d0e8559a4bd4212931b2c1e
kmhcreative <software@kmhcreative.com>
2020-10-14 12:38:23 -0500

    fixed title inclusion to use h1 and entry-title class

--- mimeo.php
+++ mimeo.php
@@ -3,17 +3,17 @@
 Plugin Name: Mimeo Shortcode
 Plugin URI: http://www.kmhcreative.com/labs/mimeo
 Description: A WordPress plugin that lets you use a shortcode to mirror the content of one page into another.
-Version: 0.1
+Version: 0.2
 Author: K.M. Hansen
 Author URI: http://www.kmhcreative.com
-License: GPLv3
+License: GPLv2 (or later)
 */
 
-/*  Copyright 2012-2014  K.M. Hansen  (email : software@kmhcreative.com)
+/*  Copyright 2012-2020  K.M. Hansen  (email : software@kmhcreative.com)
 
 	This allows you to mirror the content of one page into another by page_id.
 	
-	Why?  So a page can effectively have two "parents" and therefore be found under
+	Why?  So a page can effectively have to "parents" and therefore be found under
 	any of the parents but you still only have ONE page to maintain.
 
     Usage: [mimeo id="8" title="true"]
@@ -30,7 +30,7 @@ function get_post_page_content( $atts ) {
 			while ( $the_query->have_posts() ) {
 				$the_query->the_post();
 			        if($title == true){
-			        the_title();
+			        echo '<h1 class="entry-title">'.get_the_title().'</h1>';
 			        }
 			        the_content();
 			}
